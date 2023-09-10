97366293fc98e36c6124198ea99cf22ee4f55f34
mdabarik <mdabarik19@gmail.com>
2023-09-11 01:26:17 +0600

    added some code

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Countries from './components/Countries/Countries'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Hello Awesome!!</h1>
+      <Countries></Countries>
+    </>
+  )
+}
+
+export default App
